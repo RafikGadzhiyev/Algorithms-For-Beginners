69b8201f7e1a0daf21a73479c93f5649f31bf59a
RafikGadzhiyev <66208417+RafikGadzhiyev@users.noreply.github.com>
2021-08-08 20:49:15 +0600

    Created quick sort file
    Added script of quick sort algorithm

---
+++ quick sort/quickSort.js
@@ -0,0 +1,31 @@
+function quickSort(arr, start, finish){
+            let startTime = Date.now();
+            if(start < finish){                
+                let  p = partition(arr, start, finish);
+                quickSort(arr, start, p)
+                quickSort(arr, p + 1,finish);
+            }
+            let endTime = Date.now();
+            timer = endTime - startTime + 'ms';
+        }
+
+        function partition(arr, start, finish){
+            let pivot = (arr[start] + arr[finish]) / 2;
+            let i = start,
+                j = finish;
+
+            while (true){
+                while(arr[i] < pivot){
+                    i++
+                }
+                while(arr[j] > pivot){
+                    j--;
+                }
+            
+                if(i >= j){  
+                    return j
+                }
+                [arr[i++], arr[j--]] = [arr[j], arr[i]];
+		}
+
+	}
